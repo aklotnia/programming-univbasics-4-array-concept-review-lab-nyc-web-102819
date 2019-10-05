18c537c98d8276d766e78d3469ed21e68e5a2b46
Aaron Klotnia <aklotnia@gmail.com>
2019-10-05 18:03:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array_len = array.length
-  array_len.Times do |index|
+  array_len.times do |index|
     if array[index] == value_to_find
       return index
     end
